81f18b968f91777158e82b5b307a5f5081b29dc0
McMrCat <alexcaster07@yahoo.com>
2011-11-02 00:11:43 -0300

    Put killphysics in mod help menu

--- Commands/CmdKillPhysics.cs
+++ Commands/CmdKillPhysics.cs
@@ -31,7 +31,7 @@ namespace MCForge
     {
         public override string name { get { return "killphysics"; } }
         public override string shortcut { get { return "kp"; } }
-        public override string type { get { return ""; } }
+        public override string type { get { return "mod"; } }
         public override bool museumUsable { get { return true; } }
         public override LevelPermission defaultRank { get { return LevelPermission.Operator; } }
         public override void Use(Player p, string message)
