a9302d4494fdc829fa3b4af96e52c5d4b70b91ef
McMrCat <alexcaster07@yahoo.com>
2011-11-02 00:28:59 -0300

    Put endround in game help category

--- Commands/CmdEndRound.cs
+++ Commands/CmdEndRound.cs
@@ -7,7 +7,7 @@ namespace MCForge
     {
         public override string name { get { return "endround"; } }
         public override string shortcut { get { return "er"; } }
-        public override string type { get { return "mod"; } }
+        public override string type { get { return "game"; } }
         public override bool museumUsable { get { return true; } }
         public override LevelPermission defaultRank { get { return LevelPermission.Banned; } }
         public CmdEndRound() { }
