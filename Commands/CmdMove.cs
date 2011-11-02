3fe43ba8da4b856028860368696616a191b5269b
McMrCat <alexcaster07@yahoo.com>
2011-11-02 00:01:21 -0300

    Put /move in help mod, so it can be with /moveall

--- Commands/CmdMove.cs
+++ Commands/CmdMove.cs
@@ -23,7 +23,7 @@ namespace MCForge
     {
         public override string name { get { return "move"; } }
         public override string shortcut { get { return ""; } }
-        public override string type { get { return "other"; } }
+        public override string type { get { return "mod"; } }
         public override bool museumUsable { get { return true; } }
         public override LevelPermission defaultRank { get { return LevelPermission.Banned; } }
         public CmdMove() { }
