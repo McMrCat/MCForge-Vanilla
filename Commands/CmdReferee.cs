5fc9da764aa1de69a73021095464982cd3c9bb31
McMrCat <alexcaster07@yahoo.com>
2011-11-02 01:12:48 -0300

    Switched ref to game help

--- Commands/CmdReferee.cs
+++ Commands/CmdReferee.cs
@@ -7,7 +7,7 @@ namespace MCForge
     {
         public override string name { get { return "ref"; } }
         public override string shortcut { get { return ""; } }
-        public override string type { get { return "mod"; } }
+        public override string type { get { return "game"; } }
         public override bool museumUsable { get { return true; } }
         public override LevelPermission defaultRank { get { return LevelPermission.Operator; } }
         public CmdReferee() { }
